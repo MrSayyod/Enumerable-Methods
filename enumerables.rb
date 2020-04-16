f4b2dedbdc11e58179b69353b2c8c29eb13d0441
MrSayyod <ssayyod@gmail.com>
2020-04-16 18:44:28 +0500

    finished my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,20 +1,23 @@
-# module Enumerable
+module Enumerable
   # def my_each
-  #   (self.length).times {|n| yield(self[n])}
+  #   (is_a? Hash) ? (self.length).times {|element| yield(keys[element], self[keys[element]])} : self.length.times {|element| yield(self[element])}
   # end
-  def my_each
-    (self.length).times { |index| yield(self[index]) }
+  
+  def my_each_with_index
+    (is_a? Hash) ? (self.length).times {|element| yield(keys[element], self[keys[element]])} : self.length.times {|element| yield(self[element])}
   end
-  array1 = ["hi", 43, "potatoes", "horses", 33]
-
-  puts "my_each output\:"
-  puts ""
-  array1.my_each { |item| puts item }
-
- 
-  # def my_each_with_index
-
-  # end
+end
+  string_check = "I am a string"
+  array_check = %w(array hash 01258 symbol true 999)
+  hash_check = { :Name => "Microverse",
+    :CEO => "Ariel Camus",
+    "Current year" => 2020,
+    "Student" => "Sayyod",
+  }
+  
+  # array_check.my_each {|x| puts x }
+  # hash_check.my_each {|x, y| puts "#{x}: #{y}" }
+  # hash_check.my_each {|x| puts "#{x}" }
 
   # def my_select
 
