968d554258a70b02ad3f0ffe5242fc006ce45529
MrSayyod <ssayyod@gmail.com>
2020-04-16 19:37:52 +0500

    finished my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -4,20 +4,36 @@ module Enumerable
   # end
   
   def my_each_with_index
-    (is_a? Hash) ? (self.length).times {|element| yield(keys[element], self[keys[element]])} : self.length.times {|element| yield(self[element])}
+    self.length.times {|element| yield(self[element], element)}
   end
 end
-  string_check = "I am a string"
-  array_check = %w(array hash 01258 symbol true 999)
-  hash_check = { :Name => "Microverse",
-    :CEO => "Ariel Camus",
-    "Current year" => 2020,
-    "Student" => "Sayyod",
-  }
+  # array_check = %w(array hash 01258 symbol true 999)
+  # hash_check = { :Name => "Microverse",
+  #   :CEO => "Ariel Camus",
+  #   "Current year" => 2020,
+  #   "Student" => "Sayyod",
+  # }
   
+  # puts "Check for my_each method"
+  # puts ""
   # array_check.my_each {|x| puts x }
   # hash_check.my_each {|x, y| puts "#{x}: #{y}" }
-  # hash_check.my_each {|x| puts "#{x}" }
+  # hash_check.my_each {|x, y| puts x }
+  # puts ""
+  # puts "Compare results with .each method"
+  # puts ""
+  # array_check.each {|x| puts x }
+  # hash_check.each {|x, y| puts "#{x}: #{y}" }
+  # hash_check.each {|x, y| puts x }
+  
+  array_check = %w(array hash 01258 symbol true 999)
+  puts "Check for my_each_with_index method"
+  puts ""
+  array_check.my_each_with_index { |x, y| puts " #{x} with an index #{y}" }
+  puts ""
+  puts "Compare results with .each_with_index method"
+  puts ""
+  array_check.each_with_index { |x, y| puts " #{x} with an index #{y}" }
 
   # def my_select
 
