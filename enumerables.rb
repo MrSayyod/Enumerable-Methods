b8d1c9981fc7c6bced31ff2227dbb3633ca1b758
MrSayyod <ssayyod@gmail.com>
2020-04-17 15:21:21 +0500

    finished my_all method

--- enumerables.rb
+++ enumerables.rb
@@ -9,14 +9,37 @@ module Enumerable
   
   def my_select
     new_array=[]
-    my_each {|x| new_array.push(x) if yield(x)}
+    my_each {|element| new_array.push(element) if yield(element)}
     new_array
   end
 
-  # def my_all? 
-    
-  # end
+  def my_all?(*par) 
+    condition = true
+    if !par[0].nil?
+      my_each { |element| condition = false unless par[0] === element } 
+    elsif !block_given?
+      my_each { |element| condition = false unless element}
+    elsif block_given?
+      my_each { |element| condition = false unless yield(element)}
+    else 
+      false
+    end
+    condition
+  end
 end
+
+#   def my_all?(*arg)
+#     result = true
+#     if !arg[0].nil?
+#       my_each { |i| result = false unless arg[0] === i } # rubocop:disable Style/CaseEquality
+#     elsif !block_given?
+#       my_each { |i| result = false unless i }
+#     else
+#       my_each { |i| result = false unless yield(i) }
+#     end
+#     result
+#   end
+# end
   # array_check = %w(array hash 01258 symbol true 999)
   # hash_check = { :Name => "Microverse",
   #   :CEO => "Ariel Camus",
@@ -57,8 +80,24 @@ end
   # print %w(London, Paris, Rome, Lisbon, Geneva, Budapest).select {|x| x.include? "s"}
   # puts ""
 
-  puts [].all?
-
+  puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+  puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+  puts %w[ant bear cat].my_all?(/t/)                        #=> false
+  puts [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+  puts [nil, true, 99].my_all?                              #=> false
+  puts [].my_all?   
+  puts [3, 3, 3].my_all?(3)
+  puts [3, 3, 1].my_all?(3)
+  puts ''
+  puts ''
+  puts %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+  puts %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
+  puts %w[ant bear cat].all?(/t/)                        #=> false
+  puts [1, 2i, 3.14].all?(Numeric)                       #=> true
+  puts [nil, true, 99].all?                              #=> false
+  puts [].all?                                           #=> true
+  puts [3, 3, 3].all?(3)
+  puts [3, 3, 1].all?(3)
   # def my_any?
 
   # end
