269024b50b97bc070f863314c52678cef6c4f844
MrSayyod <ssayyod@gmail.com>
2020-05-14 14:19:47 +0500

    initialized rspec files and tests

---
+++ spec/test_spec.rb
@@ -0,0 +1,56 @@
+require './enumerables'
+describe Enumerable do
+  describe '#my_each' do
+    it 'checks each method' do
+      
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'checks each with index method' do
+      
+    end
+  end
+
+  describe '#my_select' do
+    it 'checks select method' do
+      
+    end
+  end
+
+  describe '#my_all' do
+    it 'checks all method' do
+      
+    end
+  end
+
+  describe '#my_any' do
+    it 'checks any method' do
+      
+    end
+  end
+
+  describe '#my_none' do
+    it 'checks none method' do
+      
+    end
+  end
+
+  describe '#my_count' do
+    it 'checks count method' do
+      
+    end
+  end
+
+  describe '#my_map' do
+    it 'checks map method' do
+      
+    end
+  end
+  
+  describe '#my_inject' do
+    it 'checks inject method' do
+      
+    end
+  end
+end
