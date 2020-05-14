2c3c351790576a3971e33b3c46a2251d5910a691
MrSayyod <ssayyod@gmail.com>
2020-05-14 17:25:00 +0500

    finished testing my_each_with_index method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -7,18 +7,50 @@ describe Enumerable do
   let(:empty_array) { [] }
 
   describe '#my_each' do
-    it 'checks each method' do
-      actual = empty_array
-      array_with_numbers.my_each {|x| actual.push(x + 1)}
-      expected = empty_array
-      array_with_numbers.each {|x| actual.push(x + 1)}
-      expect(actual).to eql(expected)
+    context 'when no block given' do
+      it 'should return to Enumerator' do
+        expect(array_with_numbers.my_each).to be_a(Enumerator)
+      end
+    end
+    
+    context 'if block given, checks my_each method' do
+      it 'should pass the test if results of my_each and each methods are equal' do
+        actual = empty_array
+        array_with_numbers.my_each {|x| actual.push(x + 1)}
+        expected = empty_array
+        array_with_numbers.each {|x| expected.push(x + 1)}
+        expect(actual).to eql(expected)
+      end
+      
+      it 'should return each item as requested' do
+        actual = empty_array
+        array_with_numbers.my_each {|x| actual.push(x + 1)}
+        expected = [2, 3, 4, 5, 6]
+        expect(actual).to eql(expected)
+      end
     end
   end
-
+  
   describe '#my_each_with_index' do
-    it 'checks each with index method' do
-      
+    context 'when no block given' do
+      it 'should return to Enumerator' do
+        expect(array_with_strings.my_each_with_index).to be_a(Enumerator)
+      end
+    end
+    
+    context 'if block given, checks my_each method' do
+      it 'should pass the test if results of my_each_with_index and each_with_index methods are equal' do
+        actual = empty_array
+        array_with_strings.my_each_with_index {|element, index| actual.push("[#{element}]'s index is #{index}")}
+        expected = empty_array
+        array_with_strings.each_with_index {|element, index| expected.push("#{element}'s index is #{index}")}
+        expect(actual).to eql(expected)
+        end
+      it 'should return each item as requested with indexes' do
+        actual = empty_array
+        array_with_strings.my_each_with_index {|element, index| actual.push("[#{element}]'s index is #{index}")}
+        expect(actual).to eql(["[one]'s index is 0", "[two]'s index is 1", "[three]'s index is 2", "[four]'s index is 3", "[five]'s index is 4"])
+        end
     end
   end
 
