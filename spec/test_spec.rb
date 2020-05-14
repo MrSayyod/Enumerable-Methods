a3df4b67b68c737e92ea68a68a2d685817390e73
MrSayyod <ssayyod@gmail.com>
2020-05-14 16:20:44 +0500

    finished testing my_each method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,8 +1,18 @@
 require './enumerables'
 describe Enumerable do
+  let(:array_with_numbers) { [1, 2, 3, 4, 5] }
+  let(:array_with_strings) { ['one', 'two', 'three', 'four', 'five'] }
+  let(:array_with_numbers_and_nil) { [1, 2, 3, 4, nil] }
+  let(:array_with_strings_and_numbers) { ['one', 2, 'three', 4, 'five'] }
+  let(:empty_array) { [] }
+
   describe '#my_each' do
     it 'checks each method' do
-      
+      actual = empty_array
+      array_with_numbers.my_each {|x| actual.push(x + 1)}
+      expected = empty_array
+      array_with_numbers.each {|x| actual.push(x + 1)}
+      expect(actual).to eql(expected)
     end
   end
 
