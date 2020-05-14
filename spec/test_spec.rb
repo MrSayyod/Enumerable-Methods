43faf841a48b3b330207e30b0f14f1326b6b448a
Hermela-Fikre <hermelafikre3@gmail.com>
2020-05-14 17:34:59 +0300

    Added my_any,my_none,my_all, my_map and my_count methods in the test file

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,4 +1,4 @@
-require './enumerables'
+require '../enumerables'
 describe Enumerable do
   let(:array_with_numbers) { [1, 2, 3, 4, 5] }
   let(:array_with_strings) { ['one', 'two', 'three', 'four', 'five'] }
@@ -74,34 +74,92 @@ describe Enumerable do
   end
 
   describe '#my_all' do
-    it 'checks all method' do
-      
+    context 'when passed block with condition met' do
+      it 'returns true when all items met the condition' do
+        expect(array_with_numbers.my_all? { |num| num < 6 }).to eql(true)
+      end
+    end
+
+    it 'returns true when an empty array is given' do
+      expect([].my_all?).to be true
+    end
+
+    it 'return false when any item is false' do
+      expect(array_with_numbers.my_all? { |num| num < 5 }).to eql(false)
     end
   end
 
-  describe '#my_any' do
-    it 'checks any method' do
-      
+  describe '#my_any?' do
+    context "When passed block with atleast one condition met" do
+      it 'returns true' do
+        expect(array_with_strings_and_numbers.my_any?(Integer)).to be true
+      end
+
+      it 'returns true' do
+        expect(array_with_numbers_and_nil.my_any?(nil)).to be true
+      end
     end
+
+    it 'returns true' do
+      expect(array_with_strings.my_any? { |item| item.length >=3}).to be true 
+    end
+  end
+  
+  it 'returns false' do
+    expect(array_with_strings.my_any? { |item| item.length >=6}).to be false
   end
 
-  describe '#my_none' do
-    it 'checks none method' do
-      
+  describe '#my_none?' do
+    context "When passed block with none of the condition met" do
+      it 'returns false if none of the class element is given' do
+        expect(array_with_strings_and_numbers.my_none?(Integer)).to be false
+      end
+
+      it 'returns false if none of the element is given' do
+        expect(array_with_numbers_and_nil.my_none?(nil)).to be false
+      end
+    end
+
+    it 'returns false' do
+      expect(array_with_strings.my_none? { |item| item.length >=3}).to be false 
     end
   end
+  
+  it 'returns true' do
+    expect(array_with_strings.my_none? { |item| item.length >=6}).to be true
+  end
+
+  
 
   describe '#my_count' do
-    it 'checks count method' do
-      
+    it 'Count all the elements when no parameter is given' do
+      expect(array_with_numbers.my_count).to be 5
     end
+
+    it 'should count result of the given condition when block given' do
+      expect(array_with_numbers.my_count(&:even?)).to eql 2
+    end
+
+    it 'should count arguments when argument is given' do
+    expect(array_with_numbers.my_count(2)).to eql 1
+    end
+
   end
 
   describe '#my_map' do
-    it 'checks map method' do
-      
+    context 'when passed with block' do
+      it 'returns array with each element squared' do
+        expect(array_with_numbers.my_map {|num| num**2}).to eql([1,4,9,16,25])
+      end
+    end
+
+    context 'when passed without a block' do
+      it 'returns Enumerator #my_map' do
+        expect(array_with_numbers.my_map).to be_a(Enumerator)
+      end
     end
   end
+  
   
   describe '#my_inject' do
     it 'checks inject method' do
